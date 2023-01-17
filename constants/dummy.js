9cf99b80b339997930f3d68ff0db867a4a879f0e
kdany25 <kabadany25@gmail.com>
2023-01-17 19:43:12 +0200

    feat: rearranged data

--- constants/dummy.js
+++ constants/dummy.js
@@ -3,6 +3,24 @@ import assets from "./assets";
 const NFTData = [
 	{
 		id: "NFT-01",
+		name: "Green Coins",
+		creator: "Kwizera maniple",
+		price: 7.25,
+		description:
+			"The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+		image: assets.nft02,
+		bids: [
+			{
+				id: "BID-21",
+				name: "Jessica Tan",
+				price: 7.05,
+				image: assets.person04,
+				date: "December 12, 2019 at 12:10 PM",
+			},
+		],
+	},
+	{
+		id: "NFT-02",
 		name: "Abstracto #315",
 		creator: "Kabalisa dany",
 		price: 4.25,
@@ -30,24 +48,6 @@ const NFTData = [
 				price: 4.75,
 				image: assets.person04,
 				date: "December 31, 2019 at 3:50 PM",
-			},
-		],
-	},
-	{
-		id: "NFT-02",
-		name: "Green Coins",
-		creator: "Kwizera maniple",
-		price: 7.25,
-		description:
-			"The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
-		image: assets.nft02,
-		bids: [
-			{
-				id: "BID-21",
-				name: "Jessica Tan",
-				price: 7.05,
-				image: assets.person04,
-				date: "December 12, 2019 at 12:10 PM",
 			},
 		],
 	},
